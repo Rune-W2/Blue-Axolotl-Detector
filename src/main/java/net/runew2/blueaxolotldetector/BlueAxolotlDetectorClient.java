0e4047cbdc9f6617a47af8fbaf6a91ef87928788
Rune W2 <rune.w4@gmail.com>
2023-08-03 02:10:23 -0500

    Added a radius argument that the user can add in order to detect a blue axolotl within a set radius under 128.

--- src/main/java/net/runew2/blueaxolotldetector/BlueAxolotlDetectorClient.java
+++ src/main/java/net/runew2/blueaxolotldetector/BlueAxolotlDetectorClient.java
@@ -2,28 +2,14 @@ package net.runew2.blueaxolotldetector;
 
 import com.mojang.brigadier.CommandDispatcher;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
-import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.command.CommandRegistryAccess;
-import net.minecraft.util.math.Vec3d;
 import net.runew2.blueaxolotldetector.commands.command.BadCommand;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import net.runew2.blueaxolotldetector.commands.command.*;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.Objects;
 
 public class BlueAxolotlDetectorClient implements ClientModInitializer {
     @Override
-    public void onInitializeClient() {
-        ClientCommandRegistrationCallback.EVENT.register(BlueAxolotlDetectorClient::registerCommands);
-    }
-    public static void registerCommands(CommandDispatcher<FabricClientCommandSource> dispatcher, CommandRegistryAccess registryAccess) {
-        BadCommand.register(dispatcher);
+    public void onInitializeClient(){
+
     }
 }
